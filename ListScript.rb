94b650e2e8718bc700865eda3c6b69bb9f4bdf4c
asantebuil <asante.buil@tccd.edu>
2016-06-21 20:22:54 -0500

    Update ListScript.rb
    added first name feature for automation

--- ListScript.rb
+++ ListScript.rb
@@ -15,19 +15,22 @@ load 'lib/lists.rb'
 
 #=begin
 class ListScript
-  def setup_automation your_screen_name, slug, member_num
+  def setup_automation your_screen_name, slug, mem_num
     puts 'Commencing setup'
     @screen_name = your_screen_name
-    @super_count = Integer(member_num)
     @users = []
     @follows_hash = {}
     @did_dm = {}
+    @firstname = []
+    @super_count = Integer(mem_num)
 
     test = List.new
-    obj = test.getList_members slug, your_screen_name, member_num
+
+    obj = test.getList_members slug, your_screen_name, mem_num
     obj = JSON.parse(obj.body)
 
     count = obj['users'].count
+
 
     i = 0
     while i < count do
@@ -35,6 +38,7 @@ class ListScript
       puts "Setting up for: #{@users[i]}"
       @follows_hash["#{@users[i]}"] = false
       @did_dm["#{@users[i]}"] = false
+      @firstname.insert(i, obj["users"][i]["name"].split(' ').first) #added first name
       i += 1
     end
 
@@ -70,24 +74,33 @@ class ListScript
 
         end
       #success report
-        success = (@total.to_f / @super_count.to_f) * 100.0
-        puts "Success Rate of Cycle #{i} of #{hours} is: #{success}%"
+
+        quarter_success = (@total.to_f / @super_count.to_f) * 100.0
+        puts "Percentage of followers for mini-cycle #{j+1} of 4 is: #{quarter_success}%"
       puts 'now sleeping...'
       sleep(900) #For seconds 900 = 15 minutes
       j +=  1
     end
     puts 'sending out direct messages to...'
+      k = 0
     @users.each do |user|
+
       if @follows_hash[user] == true && @did_dm[user] == false
         puts user
-        new_message.direct_message nil, user, message
+
+        complete_message = "Hi #{@firstname[k]}%2C #{message}"
+        new_message.direct_message nil, user, complete_message
         @did_dm[user] = true
       end
+      k+=1
     end
     puts 'messages sent...'
     puts 'cycle completed...'
+    @hourly_success = (@total.to_f / @super_count.to_f) * 100.0
+    puts "hourly follower percentage #{@hourly_success}"
     i += 1
     end
+    @return_success = @success
   end
 end
 
@@ -100,13 +113,22 @@ puts "Next enter the list slug (aka list name), Note* slug is case sensitive"
 slug = gets.chomp
 puts "Finally enter the number of members in your list"
 num = gets.chomp
+new_script.setup_automation username, slug, num
+
 puts "It's time to set up automation commands"
 puts " Select how many hours you want to run the script"
 hours = gets.chomp
+
 puts "Type in the the message you would like to send"
 puts "remember punctation has to be in HTMl hexa... ex. %2C = ',' or comma"
 message = gets.chomp
 
-new_script.setup_automation username, slug, num
+
 new_script.start_automation hours, message
+#success_data = new_script.start_automation hours, message collect percentage if wanted
+
+=begin
+firstname = new_script.get_firstname
+message = "Hi #{firstname}%2C #{message}"
+=end
 
